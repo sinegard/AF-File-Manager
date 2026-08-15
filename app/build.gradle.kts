daf0a0459d409e5e2f9f894bae4a473368cd5742
sinegard <301627135+sinegard@users.noreply.github.com>
2026-08-15 07:56:18 +0200

    Prepare AF File Manager 0.12.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.affilemanager.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
-        versionName = "0.11.0"
+        versionCode = 20
+        versionName = "0.12.0"
 
         buildConfigField("String", "UPDATE_REPOSITORY", "\"sinegard/AF-File-Manager\"")
 
